6f0a3a9370a8c1ec8fb1d0b5fafddf0b66a5b492
Joseph Petrich <petrich89@gmail.com>
2017-09-26 11:29:04 -0400

    Initial commit showing the problem. Run the app on a device with iOS 11, hit start recording, and hit stop recording a few seconds later. The file size of the H264 and H265 videos will be printed to the console

---
+++ HEVCDemo/VideoWriter.swift
@@ -0,0 +1,63 @@
+//
+//  VideoWriter.swift
+//  HEVCDemo
+//
+//  Created by Joseph Petrich on 9/26/17.
+//
+
+import Foundation
+import AVFoundation
+
+class VideoWriter {
+    var avAssetWriter: AVAssetWriter
+    var avAssetWriterInput: AVAssetWriterInput
+    var url:URL
+    
+    init(withVideoType type:AVVideoCodecType){
+        if #available(iOS 11.0, *) {
+            avAssetWriterInput = AVAssetWriterInput(mediaType: AVMediaType.video, outputSettings: [AVVideoCodecKey:type, AVVideoHeightKey:720, AVVideoWidthKey:1280])
+        } else {
+            avAssetWriterInput = AVAssetWriterInput(mediaType: AVMediaType.video, outputSettings: [AVVideoCodecKey:AVVideoCodecH264, AVVideoHeightKey:720, AVVideoWidthKey:1280])
+        }
+        avAssetWriterInput.expectsMediaDataInRealTime = true
+        do {
+            let directory = try VideoWriter.directoryForNewVideo()
+            if (type == AVVideoCodecType.hevc){
+                url = directory.appendingPathComponent(UUID.init().uuidString.appending(".hevc"))
+            }
+            else {
+                url = directory.appendingPathComponent(UUID.init().uuidString.appending(".mp4"))
+            }
+            avAssetWriter = try AVAssetWriter(url: url, fileType: AVFileType.mp4)
+            avAssetWriter.add(avAssetWriterInput)
+            avAssetWriter.movieFragmentInterval = kCMTimeInvalid
+        } catch {
+            fatalError("Could not initialize avAssetWriter \(error)")
+        }
+    }
+    
+    func write(sampleBuffer buffer: CMSampleBuffer) {
+        if avAssetWriter.status == AVAssetWriterStatus.unknown {
+            avAssetWriter.startWriting()
+            avAssetWriter.startSession(atSourceTime: CMSampleBufferGetPresentationTimeStamp(buffer))
+        }
+        if avAssetWriterInput.isReadyForMoreMediaData {
+            avAssetWriterInput.append(buffer)
+        }
+    }
+    
+    func stopWriting(completionHandler handler: @escaping (AVAssetWriterStatus) -> Void) {
+        avAssetWriter.finishWriting {
+            handler(self.avAssetWriter.status)
+        }
+    }
+    
+    static func directoryForNewVideo() throws -> URL {
+        let videoDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("videos")
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-yyyy"
+        let dateDir = videoDir?.appendingPathComponent(formatter.string(from:Date()))
+        try FileManager.default.createDirectory(atPath: (dateDir?.path)!, withIntermediateDirectories: true, attributes: nil)
+        return dateDir!
+    }
+}
--- HEVCDemo/ViewController.swift
+++ HEVCDemo/ViewController.swift
@@ -6,19 +6,70 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FrameExtractorDelegate {
+    var frameExtractor: FrameExtractor!
+    var videoWriterH264: VideoWriter!
+    var videoWriterH265: VideoWriter!
+    var isRecording: Bool!
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(self.notificationReceived), name: NSNotification.Name.AVCaptureSessionRuntimeError, object: nil)
+        isRecording = false
+        frameExtractor = FrameExtractor()
+        frameExtractor.delegate = self
+        videoWriterH264 = VideoWriter(withVideoType: AVVideoCodecType.h264)
+        videoWriterH265 = VideoWriter(withVideoType: AVVideoCodecType.hevc)
+    }
+    
+    @objc func notificationReceived(notification: NSNotification){
+        print(notification)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func captured(frame: CMSampleBuffer) {
+        if isRecording {
+            videoWriterH264.write(sampleBuffer: frame)
+            videoWriterH265.write(sampleBuffer: frame)
+        }
+    }
+    @IBAction func recordPressed(_ sender: Any) {
+        let button = sender as? UIButton
+        if isRecording {
+            isRecording = false
+            videoWriterH264.stopWriting(completionHandler: { (status) in
+                print("Done recording H264")
+                do {
+                    let attr = try FileManager.default.attributesOfItem(atPath: self.videoWriterH264.url.path)
+                    let fileSize = attr[FileAttributeKey.size] as! UInt64
+                    print("H264 file size = \(fileSize)")
+                } catch {
+                    
+                }
+            })
+            videoWriterH265.stopWriting(completionHandler: { (status) in
+                print("Done recording H265")
+                do {
+                    let attr = try FileManager.default.attributesOfItem(atPath: self.videoWriterH265.url.path)
+                    let fileSize = attr[FileAttributeKey.size] as! UInt64
+                    print("H265 file size = \(fileSize)")
+                } catch {
+                    
+                }
+            })
+            button?.setTitle("Start Recording", for: .normal)
+        }
+        else {
+            isRecording = true
+            button?.setTitle("Stop Recording", for: .normal)
+        }
+    }
 }
-
